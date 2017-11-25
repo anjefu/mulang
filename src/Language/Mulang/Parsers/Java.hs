597c5a78e8f5c08fdded7a3cc97ad7df18f11031
Franco Bulgarelli <franco@mumuki.org>
2017-11-25 10:16:40 -0300

    Splitting unparser into modules

--- src/Language/Mulang/Parsers/Java.hs
+++ src/Language/Mulang/Parsers/Java.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE ViewPatterns #-}
 
 
-module Language.Mulang.Parsers.Java (java, parseJava, unjava) where
+module Language.Mulang.Parsers.Java (java, parseJava) where
 
 import Language.Mulang.Ast hiding (While, Return, Equal, Lambda, Try)
 import qualified Language.Mulang.Ast as M (Expression(While, Return, Equal, Lambda, Try))
@@ -16,31 +16,6 @@ import Control.Fallible
 import Data.Maybe (fromMaybe)
 import Data.List (intercalate)
 import Data.Char (toLower)
-import Data.String (unwords)
-
-
-type Unparser = Expression -> String
-
-unjava :: Unparser
-unjava (Class name Nothing _ )           = unwords ["public class", name, "{}"]
-unjava (Class name (Just superclass) _ ) = unwords ["public class", name, "extends", superclass, "{}"]
-unjava (Interface name extends body)     = unwords ["public interface", name, unextends extends, "{", unbody body, "}"]
-unjava _                                 = ""
-
-unextends :: [Identifier] -> String
-unextends [] = ""
-unextends parents = unwords ["extends", intercalate "," parents]
-
-unbody :: Unparser
-unbody MuNull                        = ""
-unbody (Sequence members)            = unlines (map unbody members)
-unbody (TypeSignature name args typ) = unwords ["public abstract", typ, name, "(", unparam args, ");"]
-
-unparam :: [Identifier] -> String
-unparam = intercalate "," . zipWith buildParam [0..]
-
-buildParam :: Int -> Identifier -> String
-buildParam index typeParam = typeParam ++ " arg" ++ show index
 
 java :: Parser
 java = orFail . parseJava'
